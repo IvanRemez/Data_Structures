685a4c5830e3eae182670b367ed27c60d038607c
Ivan_Cydeo <remezivan1@gmail.com>
2023-03-04 16:53:50 -0500

    Collections Review started

---
+++ src/main/java/Review/Candies.java
@@ -0,0 +1,50 @@
+package Review;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Candies {
+
+    public static void main(String[] args) {
+
+        int[] candies = {2, 3, 5, 1, 3};
+        int extraCandies = 3;
+
+        boolean[] result = new boolean[5];
+
+        for (int i = 0; i < candies.length; i++) {
+            if ((candies[i] + extraCandies) >= 5) {
+                result[i] = true;
+            } else result[i] = false;
+        }
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result=new ArrayList<>();
+        int max=Arrays.stream(candies).max().getAsInt();//O(n)
+
+        // create another array
+        for (int candy : candies) { // O(n)
+            result.add(candy+extraCandies>=max);
+        }
+        return result;
+    }
+    public static List<Boolean> kidsWithCandies2(int[] candies, int extraCandies) {
+        List<Boolean> result=new ArrayList<>(); // return type
+        int max=-1;
+        //find max
+        for (int i = 0; i < candies.length; i++) {
+            if(candies[i]>max) max=candies[i];
+        }//O(n)
+
+
+        for (int i = 0; i < candies.length; i++) {
+            if(candies[i]+extraCandies>=max) result.add(true);
+            else result.add(false);
+        } // O(n)
+        // in total we have O(n) +O(n) = O(2n) which results in O(n)
+        return result;
+    }
+}
---
+++ src/main/java/day02_ArrayList/ArrayListReview.java
@@ -0,0 +1,81 @@
+package day02_ArrayList;
+
+import java.util.*;
+
+public class ArrayListReview {
+
+    public static void main(String[] args) {
+
+// Create ArrayList and a class
+
+        List<Student> students = new ArrayList<>();
+
+// add elements to ArrayList
+        students.add(new Student(1, "Jack"));
+        students.add(new Student(2, "Julia"));
+        students.add(new Student(3, "Mike"));
+        students.add(new Student(4, "Mary"));
+
+// Iterate ArrayList
+// 1. For Loop w/ get(index)
+        System.out.println("Printing with legacy For Loop--------------");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
+
+// 2. Iterator:
+// Forward Iteration
+        System.out.println("Printing with Iterator------------------");
+
+        Iterator iterator = students.listIterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+// Backward Iteration
+        System.out.println("Printing Backwards with Iterator------------------");
+
+        while (((ListIterator<?>) iterator).hasPrevious()) {
+            System.out.println(((ListIterator<?>) iterator).previous());
+        }
+
+// 3. For Each loop
+        System.out.println("Printing with For Each loop---------------------");
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+
+// 4. Lambda
+        System.out.println("Printing with Lambda For Each---------------------");
+
+        students.forEach(student -> System.out.println(student));
+
+// Sorting elements in List using Comparator Interface
+        System.out.println("Sorting w/ Comparator Interface by ID Descending");
+
+        Collections.sort(students, new sortByIdDescending());
+        students.forEach(student -> System.out.println(student));
+
+        System.out.println("Sorting w/ Comparator Interface by Name Descending");
+
+        Collections.sort(students, new sortByNameDescending());
+        students.forEach(student -> System.out.println(student));
+    }
+// Implementation of Comparator
+    static class sortByIdDescending implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o2.id - o1.id;
+    }
+}
+
+    static class sortByNameDescending implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.name.compareToIgnoreCase(o1.name);
+        }
+    }
+}
