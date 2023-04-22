a37c96e82ff3d5aa85a935b675527d0a39e1bf2d
Ivan_Cydeo <remezivan1@gmail.com>
2023-04-22 19:18:54 -0400

    SunsetViews Task done

--- src/main/java/DS_Tasks/day05/SunsetViews.java
+++ src/main/java/DS_Tasks/day05/SunsetViews.java
@@ -1,21 +1,46 @@
 package DS_Tasks.day05;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Stack;
 
 public class SunsetViews {
 
     public static void main(String[] args) {
 
+        int[] buildings = {3, 5, 4, 4, 3, 1, 3, 2};
 
+        System.out.println(sunsetViews_Stack(buildings, "EAST")); // [1, 3, 6, 7]
+        System.out.println(sunsetViews_Stack(buildings, "WEST")); // [0, 1]
     }
 
-    public ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
+    public static ArrayList<Integer> sunsetViews_Stack(int[] buildings, String direction) {
 
         Stack<Integer> stack = new Stack<>();
+        // stack to hold indices of buildings which can see sunset
+    // East part
+        int i = 0;
+        int step = 1;
+    // West part
+        if (direction.equalsIgnoreCase("west")) {
+            i = buildings.length - 1;
+            step = -1;
+        }
 
+        while (i >= 0 && i < buildings.length){
 
+            while (!stack.isEmpty() && buildings[i] >= buildings[stack.peek()]) {
+                stack.pop();
+            }
+            stack.push(i);  // pushing building INDICES into stack
+            i += step;
+        }   // need to convert to ArrayList
 
-        return new ArrayList<>();
+        if (direction.equalsIgnoreCase("west")) {
+            Collections.reverse(stack);
+        }
+
+        return new ArrayList<>(stack);
     }
 }
